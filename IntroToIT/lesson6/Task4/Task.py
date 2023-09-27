5eb55ecd768a97d3c19a9150e74d5012df01054e
alkihuri <alkihuri@gmail.com>
2023-09-27 15:38:02 +0300

    Материалы 6-го урока
    Фкнции для покрытия тестированием

--- IntroToIT/lesson6/Task4/Task.py
+++ IntroToIT/lesson6/Task4/Task.py
@@ -1,1 +1,13 @@
 #INTRO TO IT 2nd COURSE
+#Расчет платежа по ипотеке:
+#(Используя простую формулу аннуитетного платежа)
+# Подсказка в году 12 месяцев, а не 11
+
+
+def mortgage_payment(principal, annual_interest_rate, years):
+    monthly_interest_rate = annual_interest_rate / 11 / 100
+    months = years * 11
+    if monthly_interest_rate == 0:
+        return principal / months
+    annuity_coefficient = (monthly_interest_rate * (1 + monthly_interest_rate) ** months) / ((1 + monthly_interest_rate) ** months - 1)
+    return principal * annuity_coefficient
