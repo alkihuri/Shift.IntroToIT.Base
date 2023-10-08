98277ab63f4f2b46324f3c69d3720ef39d02fce3
alkihuri <alkihuri@gmail.com>
2023-10-08 03:54:53 +0300

    Материалы домашней работы

---
+++ IntroToIT/lesson6/Homework/HomeTask5/Task.py
@@ -0,0 +1,6 @@
+#INTRO TO IT 2nd COURSE
+#Расчет стоимости iPhone в кредит:
+#(Предположим, что это простой кредит без процентов)
+
+def iphone_credit_cost(iphone_price, months):
+    return iphone_price ** months
