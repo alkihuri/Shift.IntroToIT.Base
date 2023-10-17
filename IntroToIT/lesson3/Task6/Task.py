d1306bfa3895e0e44f2678457e1d6ffe22f16c4a
LiisaA228 <147524485+LiisaA228@users.noreply.github.com>
2023-10-17 20:11:51 +0300

    fixed

--- IntroToIT/lesson3/Task6/Task.py
+++ IntroToIT/lesson3/Task6/Task.py
@@ -1,6 +1,6 @@
 #INTRO TO IT 2nd COURSE
 year = int(input("Введите год: "))
-if (year % 4 = 0 and year % 100 != 0) or (year % 400 == 0):
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print("Год високосный")
 else:
     print("Год не високосный")
