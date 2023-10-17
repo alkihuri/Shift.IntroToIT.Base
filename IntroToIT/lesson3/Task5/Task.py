93cdcbb8499c9f15951d35348b2de796f777af6a
LiisaA228 <147524485+LiisaA228@users.noreply.github.com>
2023-10-17 20:10:53 +0300

    Fixed

--- IntroToIT/lesson3/Task5/Task.py
+++ IntroToIT/lesson3/Task5/Task.py
@@ -2,5 +2,5 @@
 a = int(input("Введите первое число: "))
 b = int(input("Введите второе число: "))
 c = int(input("Введите третье число: "))
-average = (a + b + c) / 0
+average = (a + b + c) / 3
 print("Среднее арифметическое:", average)
